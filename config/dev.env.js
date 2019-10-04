86002aeb9a1fd0442a01d98fb7a1e099ee67ccb6
Anna Korunska <36419787+akorunska@users.noreply.github.com>
2019-10-04 18:09:42 +0300

    Update config/dev.env.js
    Co-Authored-By: Volodymyr Palii <volodymyr.palii@gmail.com>

--- config/dev.env.js
+++ config/dev.env.js
@@ -9,7 +9,7 @@ module.exports = merge(prodEnv, {
   EXPLORE_URL: '"http://ec2-3-17-77-100.us-east-2.compute.amazonaws.com:8080/"',
   TEST_EXPLORE_URL: '"http://ec2-18-189-125-117.us-east-2.compute.amazonaws.com:8080/"',
 
-  API_URL: '"http://ec2-3-17-77-100.us-east-2.compute.amazonaws.com:8080/v2"',
+  API_URL: '"http://10.200.1.14:8090/v2"',
   TEST_API_URL: '"http://ec2-18-189-125-117.us-east-2.compute.amazonaws.com:8080/v2"',
 
   DAPP_NODE_URL: '"https://andromeda-sync.onyxpay.co:20334"',
