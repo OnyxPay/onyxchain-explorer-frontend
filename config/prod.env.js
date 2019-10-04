7b32dac8ca75f812273ac39c40d594741e8ca4e3
Anna Korunska <akorunska@gmail.com>
2019-10-04 15:56:03 +0300

    added onyxchain syncnode addresses for dev and prod configs

--- config/prod.env.js
+++ config/prod.env.js
@@ -9,8 +9,8 @@ module.exports = {
   API_URL: '"http://ec2-3-17-77-100.us-east-2.compute.amazonaws.com:8080/v2"',
   TEST_API_URL: '"http://ec2-18-189-125-117.us-east-2.compute.amazonaws.com:8080/v2"',
 
-  // DAPP_NODE_URL: '"https://dappnode1.ont.io:10334"',
-  // TEST_DAPP_NODE_URL: '"https://polaris1.ont.io:10334"',
+  DAPP_NODE_URL: '"https://andromeda-sync.onyxpay.co:20334"',
+  TEST_DAPP_NODE_URL: '"https://cepheus5.onyxpay.co:20334"',
 
   NET:true
 };
