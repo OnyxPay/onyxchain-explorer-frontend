2ad3f93242cf0769789510eeb6dc56642347ed07
Anna Korunska <akorunska@gmail.com>
2019-10-25 15:44:09 +0300

    updated prod config to use https version of backend

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,11 +3,11 @@
 module.exports = {
   NODE_ENV: '"production"',
 
-  EXPLORE_URL: '"http://ec2-3-17-77-100.us-east-2.compute.amazonaws.com:8082/"',
-  TEST_EXPLORE_URL: '"http://ec2-18-189-125-117.us-east-2.compute.amazonaws.com:8082/"',
+  EXPLORE_URL: '"https://andromeda-explorer.onyxpay.co/"',
+  TEST_EXPLORE_URL: '"https://cepheus-explorer.onyxpay.co/"',
 
-  API_URL: '"http://ec2-3-17-77-100.us-east-2.compute.amazonaws.com:8080/v2"',
-  TEST_API_URL: '"http://ec2-18-189-125-117.us-east-2.compute.amazonaws.com:8080/v2"',
+  API_URL: '"https://andromeda-explorer.onyxpay.co/v2/"',
+  TEST_API_URL: '"https://cepheus-explorer.onyxpay.co/v2/"',
 
   DAPP_NODE_URL: '"https://andromeda-sync.onyxpay.co:20334"',
   TEST_DAPP_NODE_URL: '"https://cepheus5.onyxpay.co:20334"',
